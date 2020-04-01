e2b0211de59d6d32a4f999e24f9dc0a5686b73fc
1028hayato <1028hayato.cl@gmail.com>
2020-04-01 15:19:28 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Task2 {
+	public static void main(String[] args) {
+		/*
+		 コメント
+		 コメント
+		*/
+		System.out.println("hogehoge");
+		System.out.print("hogehoge2");
+	}
+}
