20e696c4ad58cbe6173882a4d66facb462546209
1028hayato <1028hayato.cl@gmail.com>
2020-04-06 17:27:07 +0900

    再提出 -変数・定数

--- variable/src/variable/Task2.java
+++ variable/src/variable/Task2.java
@@ -4,7 +4,7 @@ public class Task2 {
 	public static void main(String args[]){
 		String name = "山田　太郎";
 		System.out.println(name + "です。");
-		final String TELEPHONE_NUMBER = "090-9999-1111";
+		final String TELEPHONE_NUMBER = "'090-9999-1111'";
 		System.out.println("私の電話番号は" + TELEPHONE_NUMBER + "です。");
 	}
 }
