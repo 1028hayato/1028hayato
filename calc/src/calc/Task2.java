788009faa7f2058451ad484e97f62c99f81c186a
1028hayato <1028hayato.cl@gmail.com>
2020-04-08 15:45:48 +0900

    再提出 - 四則演算

--- calc/src/calc/Task2.java
+++ calc/src/calc/Task2.java
@@ -2,11 +2,10 @@ package calc;
 
 public class Task2 {
 	public static void main(String args[]){
-		System.out.print("合計:");
-		int a = 10, b = 12, c = 13;
-		System.out.println(a + b + c);
-		String ava = "平均";
-		System.out.println(ava + ((a + b + c) / 3));
-		System.out.println(ava +((a + b + c) / 3.0));
+		int a = 10, b = 12, c = 13,	total = a + b + c, ava = total / 3;
+		double ava2 = total / 3.0;
+		System.out.println("合計:" + total);
+		System.out.println("平均(3で割った時):"+ ava);
+		System.out.print("平均(3.0で割った時):"+ ava2);
 	}
 }
--- calc/src/calc/Task3.java
+++ calc/src/calc/Task3.java
@@ -7,16 +7,14 @@ public interface Task3{
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("3つの数字の合計と平均を出力します。1つ目の数字を入力してください。");
 		int x = sc.nextInt();
-		System.out.println("2つ目の数字を入力してください。");
 		int y = sc.nextInt();
-		System.out.println("3つ目の数字を入力してください。");
 		int z = sc.nextInt();
-		String total = "合計:",ava = "平均";
-		System.out.println(total + (x + y + z));
-		System.out.println(ava +"(3で割った時):"+ (x + y + z) / 3);
-		System.out.print(ava + "(3.0で割った時):"+ (x + y + z) / 3.0);
+		int total = x + y + z, ava = total / 3;
+		double ava2 = total / 3.0;
+		System.out.println("合計:" + total);
+		System.out.println("平均(3で割った時):"+ ava);
+		System.out.println("平均(3.0で割った時):"+ ava2);
 		sc.close();
 	}
 }
