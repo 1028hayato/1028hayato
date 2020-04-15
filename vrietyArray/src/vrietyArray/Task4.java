1e49ea49b7a37d0a7c85e69117b34b894a6dbb88
1028hayato <1028hayato.cl@gmail.com>
2020-04-15 18:23:17 +0900

    再提出 - いろいろな配列

--- vrietyArray/src/vrietyArray/Task4.java
+++ vrietyArray/src/vrietyArray/Task4.java
@@ -7,11 +7,11 @@ import java.util.HashMap;
 public class Task4 {
 	public static void main(String args[] ) {
 		ArrayList<Integer> numbers = new ArrayList<>();
-		HashMap<Integer,ArrayList<Integer>> even = new HashMap<>();
-		even.put(1,numbers);
 		ArrayList<Integer> numbers2 = new ArrayList<>();
-		HashMap<Integer,ArrayList<Integer>> odd = new HashMap<>();
-		odd.put(2,numbers2);
+		HashMap<String,ArrayList<Integer>> even_odd = new HashMap<>();
+		even_odd.put("偶数:"+" ",numbers);
+		even_odd.put("奇数:"+ " ",numbers2);
+
 		int [] num = {21,3,32,6,99,72,78,51,1,26,87,11,48,60,29,5,46,74,14,193,215,83};
 		for(int i = 0; i < num.length; i++) {
 			if(num[i] % 2 == 0) {
@@ -20,7 +20,6 @@ public class Task4 {
 				numbers2.add(num[i]);
 			}
 		}
-		System.out.println("{" + "偶数" + " " + "=" + even.get(1));
-		System.out.println("奇数" + " " + "=" + odd.get(2) + "}");
+		System.out.println(even_odd);
 	}
 }
