ae551148e81080b830ec5962ca03865d9e6d64a5
1028hayato <1028hayato.cl@gmail.com>
2020-04-20 17:56:36 +0900

    再提出 - インスタンス＆クラス

--- instance/src/instance/Person2.java
+++ instance/src/instance/Person2.java
@@ -3,9 +3,9 @@ package instance;
 //Task1のでPersonクラスがすでにある為、Person2にしています
 public class Person2 {
 	//名前
-	String name = "福島隼人";
+	String name;
 	//年齢
-	int age = 29;
+	int age;
 
 	public  void myProfile() {
 		System.out.println("私の名前は" + this.name + "です。");
--- instance/src/instance/Task3.java
+++ instance/src/instance/Task3.java
@@ -4,6 +4,8 @@ class Task3 {
 
 	public static void main(String[]args) {
 		Person2 person2 = new Person2(); //Person2のインスタンス
+		person2.name = "福島隼人";
+		person2.age = 29;
 		person2.myProfile(); // Person2クラスのmyProfileメソッドの出力
 	}
 }
