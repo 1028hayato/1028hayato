cf5fbc2d93fefa076cbd08a600e772e4b3f2721e
1028hayato <1028hayato.cl@gmail.com>
2020-05-02 18:04:56 +0900

    課題提出 - サーブレットとJSP

--- Servlet_jsp/src/practice/Task4.java
+++ Servlet_jsp/src/practice/Task4.java
@@ -13,17 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 public class Task4 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
-		Date today = new Date();
-		String[] fortunes = {"大吉", "中吉", "小吉", "末吉", "凶", "大凶"};
-		Random random = new Random();
-		String fortune = fortunes[random.nextInt(6)];
+         response.setContentType("text/html; charset=UTF-8");
+		 Date today = new Date();
+		 String[] fortunes = {"大吉", "中吉", "小吉", "末吉", "凶", "大凶"};
+		 Random random = new Random();
+		 String fortune = fortunes[random.nextInt(6)];
 
-		FortuneBean fortunebean = new FortuneBean();
-		fortunebean.setToday(today);
-		fortunebean.setFortune(fortune);
-        request.setAttribute("占う", fortunebean);
+		 FortuneBean fortunebean = new FortuneBean();
+		 fortunebean.setToday(today);
+		 fortunebean.setFortune(fortune);
+		 request.setAttribute("占う", fortunebean);
 
-		request.getRequestDispatcher("/fortune_result.jsp").forward(request, response);
+		 request.getRequestDispatcher("/fortune_result.jsp").forward(request, response);
 	}
 }
