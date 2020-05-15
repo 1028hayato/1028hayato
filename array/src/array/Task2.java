bfc32328b52deb774b3912f571ee7e90c0640646
1028hayato <1028hayato.cl@gmail.com>
2020-05-15 16:06:28 +0900

    再提出 - 配列

--- array/src/array/Task2.java
+++ array/src/array/Task2.java
@@ -8,20 +8,18 @@ public class Task2 {
 			numbers[i]= a--;
 			//配列に数字を代入
 		}
-		for (int i = numbers.length -1; i >= 0 ; i--) {
-			if (i >= 1) {
-				System.out.print(numbers[i] + ",");
-			}else{
-				System.out.println(numbers[i]);
-				//7～1の数字を出力
-			}
-		}
 		for (int i = 0; i < numbers.length; i++) {
 			if (i < numbers.length -1) {
 				System.out.print(numbers[i] + ",");
 			}else{
 				System.out.println(numbers[i]);
-				//1～7の数字を出力
+			}
+		}
+		for (int i = numbers.length -1; i >= 0 ; i--) {
+			if (i >= 1) {
+				System.out.print(numbers[i] + ",");
+			}else{
+				System.out.println(numbers[i]);
 			}
 		}
 	}
